f0f0268b4e6e1d0cc390d6f9c4450a791fbb1a1b
Thiago Villa <thiagovilla89@gmail.com>
2021-02-22 20:18:28 -0300

    fix (app test): update `app-root` selector

--- src/app.spec.js
+++ src/app.spec.js
@@ -1,19 +1,19 @@
-describe('App', function () {
-    var $compile
-    var $rootScope
+describe("App", function () {
+  var $compile;
+  var $rootScope;
 
-    beforeEach(module('app', 'app.html'))
+  beforeEach(module("app", "app.html"));
 
-    beforeEach(inject(function (_$compile_, _$rootScope_) {
-        $compile = _$compile_
-        $rootScope = _$rootScope_
-    }))
+  beforeEach(inject(function (_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
 
-    it('replaces directive with appropriate content', function () {
-        var element = $compile("<app></app>")($rootScope)
+  it("replaces directive with appropriate content", function () {
+    var element = $compile("<app-root></app-root>")($rootScope);
 
-        $rootScope.$digest();
+    $rootScope.$digest();
 
-        expect(element.html()).toContain('<h1>Hello World</h1>')
-    })
-})
+    expect(element.html()).toContain("<h1>Hello World</h1>");
+  });
+});
