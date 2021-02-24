7b1401048111d472c9320a679e437885943e1ea7
Thiago Villa <thiagovilla89@gmail.com>
2021-02-23 21:03:17 -0300

    feat(router): install and test legacy (0.4.2) `ui-router`

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,11 @@
-var app = angular.module("app", ["app.layout"]);
+var app = angular.module("app", ["ui.router", "app.layout"]);
+
+app.config(function ($stateProvider) {
+  var helloState = {
+    name: "hello",
+    url: "/hello",
+    template: "<h3>Hello</h3>",
+  };
+
+  $stateProvider.state(helloState);
+});
